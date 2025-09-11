40ac285a06bba9fa278c742475c4644a5637e532
pmoguel <pmoguel@centro.edu.mx>
2025-09-11 14:42:58 -0600

    Creacion: Lineas negras sobre fondo blanco, base de la composicion

--- Jueves-11-Sep/app.js
+++ Jueves-11-Sep/app.js
@@ -1,35 +1,16 @@
 const canvas = document.getElementById('lienzo');
 const ctx = canvas.getContext('2d');
 
-const lineScale = 0.6;
+const totalLines = 8;
+const spacing = canvas.height / (totalLines + 1);
 
-const totalBars = 23;
-const barWidth = 8 * lineScale;
-const spacing = 19.8 * lineScale;
-const baseY = canvas.height / 2;
+ctx.strokeStyle = '#black';
+ctx.lineWidth = 1;
 
-const totalWidth = (totalBars - 1) * spacing;
-const startX = canvas.width / 2 - totalWidth / 2;
-
-ctx.strokeStyle = '#ffffff';
-ctx.lineWidth = barWidth;
-ctx.lineCap = 'round';
-
-const barHeights = [
-  15, 60, 20, 42, 10, 52, 75, 20, 20, 54,
-  32, 85, 85, 30, 20, 43, 54, 54, 54, 44,
-  10, 19, 10
-];
-const scaledHeights = barHeights.map(h => h * lineScale);
-
-scaledHeights.forEach((height, i) => {
-  const x = startX + i * spacing;
-  const yStart = baseY - height / 2;
-  const yEnd = baseY + height / 2;
-
+for (let i = 1; i <= totalLines; i++) {
+  const y = Math.round(i * spacing) + 0.5;
   ctx.beginPath();
-  ctx.moveTo(x, yStart);
-  ctx.lineTo(x, yEnd);
+  ctx.moveTo(30, y);
+  ctx.lineTo(canvas.width - 30, y);
   ctx.stroke();
-});
-
+}
