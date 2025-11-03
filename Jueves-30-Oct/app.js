a979e268e9b294886c8f44a1aadd346c480b70c9
pmoguel <pmoguel@centro.edu.mx>
2025-11-02 22:15:02 -0600

    Agregado: Comments

--- Jueves-30-Oct/app.js
+++ Jueves-30-Oct/app.js
@@ -1,21 +1,25 @@
+// Espera a que el DOM esté listo
 document.addEventListener('DOMContentLoaded', () => {
 
     const container = document.querySelector('.screen-container');
     
+    // Selecciona TODOS los elementos que tienen el atributo 'data-depth'
     const layers = document.querySelectorAll('.parallax-item, .background-text');
 
     container.addEventListener('mousemove', (e) => {
-        
+
         const sensitivity = 14;
         const mouseX = (e.clientX - (container.offsetWidth / 2)) / sensitivity;
         const mouseY = (e.clientY - (container.offsetHeight / 2)) / sensitivity;
 
+        // 2. Mover cada capa
         layers.forEach(layer => {
             const depth = parseFloat(layer.dataset.depth);
 
             const moveX = -(mouseX * depth);
             const moveY = -(mouseY * depth);
 
+            // 3. Aplicar la animación con GSAP
             gsap.to(layer, {
                 x: moveX,
                 y: moveY,
